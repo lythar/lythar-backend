14944fc2709e31210d2477fa7dc4e509cbb59626
mufaroxyz <mufaro.priv@gmail.com>
2024-01-30 14:47:12 +0100

    organize code and add some shit 👍

--- lythar-backend/Program.cs
+++ lythar-backend/Program.cs
@@ -1,68 +1,20 @@
-﻿using System.Net;
-using System.DirectoryServices.Protocols;
+﻿using lythar_backend.ldap;
 
-namespace Program
+namespace lythar_backend
 {
-    public class LdapConnect
-    {
-
-        LdapConnection ldapConnection;
-
-        public LdapConnect()
-        {
-            try
-            {
-                LdapDirectoryIdentifier ldi = new LdapDirectoryIdentifier("192.168.1.59", 389);
-                ldapConnection = new LdapConnection(ldi);
-                Console.WriteLine("[LDAP] : Created Connetion");
-                ldapConnection.AuthType = AuthType.Basic;
-                ldapConnection.SessionOptions.ProtocolVersion = 3;
-
-                NetworkCredential nc = new NetworkCredential("cn=admin,dc=mufaro,dc=com",
-                    "admin_pass"); //password
-
-                ldapConnection.Bind(nc);
-                Console.WriteLine("[LDAP] : Authenticated successfully");
-            }
-            catch (LdapException e)
-            {
-                Console.WriteLine("\r\nUnable to login:\r\n\t" + e.Message);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("\r\nUnexpected exception occured:\r\n\t" + e.GetType() + ":" + e.Message);
-            }
-        }
-
-        public void CreateUser(string username)
-        {
-            int id = 1;
-
-            AddRequest addme = new AddRequest(String.Format(@"cn={0}, ou=Users,dc=mufaro,dc=com", username));
-            addme.Attributes.Add(new DirectoryAttribute("objectclass", new object[] { "inetorgPerson" }));
-            addme.Attributes.Add(new DirectoryAttribute("givenName", "new"));
-            addme.Attributes.Add(new DirectoryAttribute("sn", "user"));
-            addme.Attributes.Add(new DirectoryAttribute("userid", id.ToString()));
-            addme.Attributes.Add(new DirectoryAttribute("description", "A test user"));
-            addme.Attributes.Add(new DirectoryAttribute("userPassword", "password"));
-            try
-            {
-                var response = ldapConnection.SendRequest(addme);
-                Console.WriteLine($"[LDAP] Created User {username}");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
             LdapConnect ldap = new LdapConnect();
-            ldap.CreateUser("TestUser");
+
+            string username = "mufaro";
+            string password = "mufaro";
+            bool isCorrect = User.Authenticate(ldap.ldapConnection, username, password);
+
+            Console.WriteLine(isCorrect);
+
+            ldap.CloseConnection();
         }
     }
 }
---
+++ lythar-backend/ldap/LdapConnect.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+using System.DirectoryServices.Protocols;
+using static System.String;
+
+namespace lythar_backend.ldap
+{
+    public class LdapConnect
+    {
+
+        public LdapConnection ldapConnection;
+        public LdapDirectoryIdentifier RootEntry = new LdapDirectoryIdentifier("192.168.1.59", 389);
+
+        public LdapConnect()
+        {
+            try
+            {
+                ldapConnection = new LdapConnection(RootEntry);
+                Console.WriteLine("[LDAP] : Created Connetion");
+                ldapConnection.AuthType = AuthType.Basic;
+                ldapConnection.SessionOptions.ProtocolVersion = 3;
+
+                NetworkCredential nc = new NetworkCredential("cn=admin,dc=mufaro,dc=com",
+                    "admin_pass"); //password
+
+                ldapConnection.Bind(nc);
+                Console.WriteLine("[LDAP] : Authenticated successfully");
+            }
+            catch (LdapException e)
+            {
+                Console.WriteLine("\r\nUnable to login:\r\n\t" + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\r\nUnexpected exception occured:\r\n\t" + e.GetType() + ":" + e.Message);
+            }
+
+        }
+
+        public void CloseConnection()
+        {
+            ldapConnection.Dispose();
+        }
+    }
+
+}
