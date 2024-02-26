05fd99e639735150dce1d750d4fee5ee8f6e1f84
Kyiro <54126666+Kyiro@users.noreply.github.com>
2024-02-26 18:21:05 +0100

    Simplify attachments

---
+++ LytharBackend/Migrations/20240226172039_SimplifyAttachments.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LytharBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class SimplifyAttachments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Attachments_Messages_AttachedToMessageId",
+                table: "Attachments");
+
+            migrationBuilder.DropColumn(
+                name: "AttachedToId",
+                table: "Attachments");
+
+            migrationBuilder.RenameColumn(
+                name: "AttachedToMessageId",
+                table: "Attachments",
+                newName: "MessageId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Attachments_AttachedToMessageId",
+                table: "Attachments",
+                newName: "IX_Attachments_MessageId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Attachments_Messages_MessageId",
+                table: "Attachments",
+                column: "MessageId",
+                principalTable: "Messages",
+                principalColumn: "MessageId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Attachments_Messages_MessageId",
+                table: "Attachments");
+
+            migrationBuilder.RenameColumn(
+                name: "MessageId",
+                table: "Attachments",
+                newName: "AttachedToMessageId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Attachments_MessageId",
+                table: "Attachments",
+                newName: "IX_Attachments_AttachedToMessageId");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AttachedToId",
+                table: "Attachments",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Attachments_Messages_AttachedToMessageId",
+                table: "Attachments",
+                column: "AttachedToMessageId",
+                principalTable: "Messages",
+                principalColumn: "MessageId");
+        }
+    }
+}
--- LytharBackend/Models/Attachment.cs
+++ LytharBackend/Models/Attachment.cs
@@ -3,8 +3,6 @@
 public class Attachment
 {
     public Guid Id { get; set; }
-    public Message? AttachedTo { get; set; } = null!;
-    public int? AttachedToId { get; set; }
     public required string Name { get; set; }
     public required string CdnNamespace { get; set; }
     public required string CdnUrl { get; set; }
